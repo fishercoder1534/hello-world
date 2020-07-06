69e20b1e7d7b37823d297dec8b7c9b5fa3e1dda4
Fisher Coder <fishercoder@gmail.com>
2020-07-06 15:11:09 -0700

    use directive to show data in a page

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ "Title: " + title + "\t" + getTitle() }}</h2>'
+    template: `
+        <h2>{{ "Title: " + title + "\t" + getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
     title = "List of Courses";
@@ -10,4 +17,6 @@ export class CoursesComponent {
     getTitle() {
         return this.title;
     }
+
+    courses = ['course1', 'course2', 'course3']
 }
