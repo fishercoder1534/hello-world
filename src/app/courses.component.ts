eda69f5f1de47017aa3d5268dce334e366172a51
Fisher Coder <fishercoder@gmail.com>
2020-07-06 16:48:23 -0700

    display an iamge on the Internet

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -10,13 +10,16 @@ import { CoursesService } from './courses.service';
                 {{ course }}
             </li>
         </ul>
+        <img src="{{ imageUrl }}"/>
     `
 })
 export class CoursesComponent {
     title = "List of Courses";
     courses;
+    imageUrl;
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
+        this.imageUrl = "https://i.ytimg.com/vi/DiOTbkLZeK0/maxresdefault.jpg";
     }
 }
