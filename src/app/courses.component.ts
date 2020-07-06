6f2ac0d70315ebbf1c5c7a769cc33760e4f18865
Fisher Coder <fishercoder@gmail.com>
2020-07-06 16:14:27 -0700

    use a simple service to get data to display

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ "Title: " + title + "\t" + getTitle() }}</h2>
+        <h2>{{ "Title: " + title }}</h2>
         <ul>
             <li *ngFor="let course of courses">
                 {{ course }}
@@ -13,10 +14,10 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
     title = "List of Courses";
+    courses;
 
-    getTitle() {
-        return this.title;
+    constructor() {
+        let service = new CoursesService();
+        this.courses = service.getCourses();
     }
-
-    courses = ['course1', 'course2', 'course3']
 }
