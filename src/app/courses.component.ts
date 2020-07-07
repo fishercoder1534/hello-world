c92fc3240b24fc37fa940693044d7515a87c4242
Fisher Coder <fishercoder@gmail.com>
2020-07-07 10:59:29 -0700

    add class binding example

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -19,7 +19,7 @@ import { CoursesService } from './courses.service';
             </tr>
         </table>
 
-        <button class="btn btn-primary">Save</button>
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 export class CoursesComponent {
@@ -27,6 +27,7 @@ export class CoursesComponent {
     courses;
     imageUrl;
     colSpan = 2;
+    isActive = true;
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
