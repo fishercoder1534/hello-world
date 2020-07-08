4af071a981edec8f3c763eb00019dd1b678a7bf3
Fisher Coder <fishercoder@gmail.com>
2020-07-08 16:58:44 -0700

    add another complete template-drive form example

---
+++ src/app/course-form/course-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-form',
+  templateUrl: './course-form.component.html',
+  styleUrls: ['./course-form.component.css']
+})
+export class CourseFormComponent {
+
+  log(x) {
+    console.log(x);
+  }
+
+  submit(f) {
+    console.log(f);
+  }
+
+  categoryList = [
+    {id: 1, name: "Development"},
+    {id: 2, name: "Art"},
+    {id: 3, name: "Science"},
+  ];
+
+}
