8c06d9b05e0aea9ddf1678aad160fe92323c6f15
Fisher Coder <fishercoder@gmail.com>
2020-07-09 09:43:23 -0700

    add some simple formControl to signup-form component

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
@@ -6,5 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent {
-
+  form = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl()
+  })
 }
