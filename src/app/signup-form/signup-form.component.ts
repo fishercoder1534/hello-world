e5fb7ce5ef38a0bdb692aec0d8d38656336f3d18
Fisher Coder <fishercoder@gmail.com>
2020-07-09 10:35:25 -0700

    add custom validations

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
 
 @Component({
   selector: 'app-signup-form',
@@ -11,7 +12,9 @@ export class SignupFormComponent {
     username: new FormControl('', 
     [ 
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace,
+      UsernameValidators.maxLength
     ]
     ),
     password: new FormControl('', Validators.required)
---
+++ src/app/signup-form/username.validators.ts
@@ -0,0 +1,17 @@
+import { AbstractControl, ValidationErrors } from "@angular/forms";
+
+export class UsernameValidators {
+    static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
+        if ((control.value as string).indexOf(' ') >= 0) {
+            return { cannotContainSpace: true };
+        }
+        return null;
+    }
+
+    static maxLength(control: AbstractControl) : ValidationErrors | null {
+        if ((control.value as string).length > 30) {
+            return { maxLength: true };
+        }
+        return null;
+    }
+}
